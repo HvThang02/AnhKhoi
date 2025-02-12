23283c9d4199074a34aa42f5b5cb8205092928b5
HvThang02 <88171110+HvThang02@users.noreply.github.com>
2025-02-12 22:49:15 +0700

    Fix image paths in Home component to use relative URLs

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,146 +2,146 @@ import { Carousel } from "antd";
 
 const slides = [
   {
-    img: "/images/slides/1.png",
+    img: "./images/slides/1.png",
     alt: "Banner slide 1",
   },
   {
-    img: "/images/slides/2.png",
+    img: "./images/slides/2.png",
     alt: "Banner slide 2",
   },
   {
-    img: "/images/slides/3.png",
+    img: "./images/slides/3.png",
     alt: "Banner slide 3",
   },
   {
-    img: "/images/slides/4.png",
+    img: "./images/slides/4.png",
     alt: "Banner slide 4",
   },
 ];
 
 const partners = [
   {
-    img: "/images/partners/1.png",
+    img: "./images/partners/1.png",
     alt: "Partner 1",
   },
   {
-    img: "/images/partners/2.png",
+    img: "./images/partners/2.png",
     alt: "Partner 2",
   },
   {
-    img: "/images/partners/3.png",
+    img: "./images/partners/3.png",
     alt: "Partner 3",
   },
   {
-    img: "/images/partners/4.png",
+    img: "./images/partners/4.png",
     alt: "Partner 4",
   },
   {
-    img: "/images/partners/5.png",
+    img: "./images/partners/5.png",
     alt: "Partner 5",
   },
   {
-    img: "/images/partners/6.png",
+    img: "./images/partners/6.png",
     alt: "Partner 6",
   },
   {
-    img: "/images/partners/7.png",
+    img: "./images/partners/7.png",
     alt: "Partner 7",
   },
   {
-    img: "/images/partners/8.png",
+    img: "./images/partners/8.png",
     alt: "Partner 8",
   },
   {
-    img: "/images/partners/9.png",
+    img: "./images/partners/9.png",
     alt: "Partner 9",
   },
   {
-    img: "/images/partners/10.png",
+    img: "./images/partners/10.png",
     alt: "Partner 10",
   },
   {
-    img: "/images/partners/11.png",
+    img: "./images/partners/11.png",
     alt: "Partner 11",
   },
   {
-    img: "/images/partners/12.png",
+    img: "./images/partners/12.png",
     alt: "Partner 12",
   },
   {
-    img: "/images/partners/13.png",
+    img: "./images/partners/13.png",
     alt: "Partner 13",
   },
   {
-    img: "/images/partners/14.png",
+    img: "./images/partners/14.png",
     alt: "Partner 14",
   },
   {
-    img: "/images/partners/15.png",
+    img: "./images/partners/15.png",
     alt: "Partner 15",
   },
   {
-    img: "/images/partners/16.png",
+    img: "./images/partners/16.png",
     alt: "Partner 16",
   },
   {
-    img: "/images/partners/17.png",
+    img: "./images/partners/17.png",
     alt: "Partner 17",
   },
   {
-    img: "/images/partners/18.png",
+    img: "./images/partners/18.png",
     alt: "Partner 18",
   },
   {
-    img: "/images/partners/19.png",
+    img: "./images/partners/19.png",
     alt: "Partner 19",
   },
   {
-    img: "/images/partners/20.png",
+    img: "./images/partners/20.png",
     alt: "Partner 20",
   },
   {
-    img: "/images/partners/21.png",
+    img: "./images/partners/21.png",
     alt: "Partner 21",
   },
   {
-    img: "/images/partners/22.png",
+    img: "./images/partners/22.png",
     alt: "Partner 22",
   },
   {
-    img: "/images/partners/23.png",
+    img: "./images/partners/23.png",
     alt: "Partner 23",
   },
   {
-    img: "/images/partners/24.png",
+    img: "./images/partners/24.png",
     alt: "Partner 24",
   },
   {
-    img: "/images/partners/25.png",
+    img: "./images/partners/25.png",
     alt: "Partner 25",
   },
   {
-    img: "/images/partners/26.png",
+    img: "./images/partners/26.png",
     alt: "Partner 26",
   },
   {
-    img: "/images/partners/27.png",
+    img: "./images/partners/27.png",
     alt: "Partner 27",
   },
   {
-    img: "/images/partners/28.png",
+    img: "./images/partners/28.png",
     alt: "Partner 28",
   },
   {
-    img: "/images/partners/29.png",
+    img: "./images/partners/29.png",
     alt: "Partner 29",
   },
   {
-    img: "/images/partners/30.png",
+    img: "./images/partners/30.png",
     alt: "Partner 30",
   },
   {
-    img: "/images/partners/31.png",
+    img: "./images/partners/31.png",
     alt: "Partner 31",
   },
 ];
@@ -218,7 +218,7 @@ const Home = () => {
       <div className="p-4 flex flex-col gap-20 items-center justify-center mb-20">
         <div className="grid grid-cols-2 pt-14 max-w-[1400px]">
           <div className="flex items-center justify-center">
-            <img src="/images/banner/about.png" className="w-[500px] " />
+            <img src="./images/banner/about.png" className="w-[500px] " />
           </div>
           <div className="flex flex-col ">
             <h2 className="text-3xl font-bold mb-6">Giới Thiệu Chung</h2>
@@ -376,7 +376,7 @@ const Home = () => {
         {/* Tại Sao Chọn Chúng Tôi */}
         <div className="grid grid-cols-2 max-w-[1400px] gap-10">
           <div className="flex items-end justify-end">
-            <img src="/images/banner/why-us.png" className="w-[400px] " />
+            <img src="./images/banner/why-us.png" className="w-[400px] " />
           </div>
           <div className="max-w-6xl  px-6 lg:px-8">
             <h2 className="text-3xl font-bold text-gray-800 mb-8">
